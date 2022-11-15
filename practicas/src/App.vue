02a71dba67d2c45c7923dbcaf1bb6b1b57841af1
CarlosV <0223996@up.edu.mx>
2022-11-14 20:31:46 -0600

    primera parte practica 4

--- practicas/src/App.vue
+++ practicas/src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <ion-router-outlet/>
   </ion-app>
 </template>
 
 <script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth"
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
     IonRouterOutlet
+  },
+  created(){
+    const auth = getAuth()
+
+    signInWithEmailAndPassword(auth, "ralejandrobm@gmail.com", "alex11").then((userCredential) => {
+      const user = userCredential.user
+      if(user == null)
+      {
+        console.log("no autenticado")
+      }
+      else
+      {
+        console.log("autenticado correctamente",user)
+      }
+    })
   }
 });
 </script>
--- practicas/src/main.js
+++ practicas/src/main.js
@@ -23,9 +23,32 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+//////////////////////////////////////////////////////////////
+//////////////////////FIREASE//////////////////////////////
+//////////////////////////////////////////////////
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCscxX4ap7uBVChxMXAny2RyZI4HFwFkxk",
+  authDomain: "practica4-70adb.firebaseapp.com",
+  projectId: "practica4-70adb",
+  storageBucket: "practica4-70adb.appspot.com",
+  messagingSenderId: "19746811236",
+  appId: "1:19746811236:web:ae58d806ede447b8fb1e71"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(appFirebase);
   
 router.isReady().then(() => {
   app.mount('#app');
